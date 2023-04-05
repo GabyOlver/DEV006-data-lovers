192b713ba59c1105ef85a9a3974aa6a52d6b641e
GabyOlver <127071903+GabyOlver@users.noreply.github.com>
2023-04-04 18:41:58 -0600

    Informacion si null

--- src/main.js
+++ src/main.js
@@ -13,15 +13,14 @@ characters.forEach(function (character) {
   characterContainer.innerHTML = `
 <h3>ID: ${character.id}</h3>
 <img class="character-img" src="${character.img}" alt="Imagen de ${character.name}">
-<h3>Nombre: ${character.name != null ? character.name : ""}</h3>
-<p>Fecha de Nacimiento: ${character.birth}</p>
+<h3>Nombre: ${character.name != null ? character.name : "N/A"}</h3>
+<p>Fecha de Nacimiento: ${character.birth != null ? character.birth : "N/A"}</p>
 <p>Fecha de Muerte: ${character.death != null ? character.death : "N/A"}</p>
-<p>Especie: ${character.species}</p>
-<p>Genero: ${character.gender}</p>
-<p>Patronus: ${character.patronus}</p>
-<p>Casa: ${character.house}</p>
-<p>Grupos a los que esta asociado: ${character.associated_groups}</p>
-<p>Libros en los que aparece: ${character.books_featured_in}</p>
+<p>Especie: ${character.species != null ? character.species : "N/A"}</p>
+<p>Genero: ${character.gender != null ? character.gender : "N/A"}</p>
+<p>Patronus: ${character.patronus != null ? character.patronus : "N/A"}</p>
+<p>Casa: ${character.house != null ? character.house : "N/A"}</p>
+<p>Libros en los que aparece: ${character.books_featured_in != null ? character.books_featured_in : "N/A"}</p>
 `;
   charactersContainer.appendChild(characterContainer);
 });
