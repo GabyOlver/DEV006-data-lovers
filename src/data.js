b3b37d8c787d192ad4849ac18324a7708de5f944
Clau2210 <claudiauriascaudillo@gmail.com>
2023-04-06 10:55:21 -0600

    Cambios6

--- src/data.js
+++ src/data.js
@@ -1,12 +1,12 @@
-// estas funciones son de ejemplo
-import data from "../src/data/harrypotter"
+// // estas funciones son de ejemplo
+// //import data from "../src/data/harrypotter"
 
-export const filtrarData = () => {
-  // eslint-disable-next-line no-console
-  console.log(data);
-  return 'example';
-};
+// //export const filtrarData = () => {
+//   // eslint-disable-next-line no-console
+//   //console.log(data);
+//  // return 'example';
+// //};
 
-export const anotherExample = () => {
-  return 'OMG';
-};
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
